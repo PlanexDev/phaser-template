7a3de3bffc8ac3b692d3cdbf389fbb7035a7501b
PlanexDev <planexdev@gmail.com>
2022-06-27 14:39:04 -0700

    chore: remove unused dependency "js-yaml"

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const yaml = require("js-yaml");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
